f03769687c3f52707689fc6c2d0c553a73750314
Sindre Bakken <sindre.bakken.naesset@gmail.com>
2022-06-16 15:16:29 +0200

    shot in the dark

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
+
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
@@ -41,7 +41,7 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new Dotenv(),
-    new ESLintPlugin(),
+
   ],
   resolve: {
     extensions: ['*', '.js'],
